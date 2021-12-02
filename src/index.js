c3135e683003f35bd1bfb649f93da823bcb67541
markchamberlain99 <45742521+markchamberlain99@users.noreply.github.com>
2021-12-01 22:55:44 -0500

    added fname and lname

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Mark";
+const fname = "Mark";
+const lname = "Doe";
 const luckyNumber = 7;
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
+    <h1>Hello {fname + " " + lname}!</h1>
     <p>My lucky number is: {luckyNumber}</p>
   </div>,
   document.getElementById("root")
